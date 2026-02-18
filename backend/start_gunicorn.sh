651ece1bac28c2ba3ab411b3b19a07c68d3cdce3
dev <dev.digitify@gmail.com>
2026-02-18 12:06:41 +0100

    prod

--- backend/start_gunicorn.sh
+++ backend/start_gunicorn.sh
@@ -2,4 +2,4 @@
 # Run Gunicorn with env loaded. Use: ./start_gunicorn.sh   or   nohup ./start_gunicorn.sh > gunicorn.log 2>&1 &
 cd "$(dirname "$0")"
 source load_env.sh
-exec python3 -m gunicorn --bind 0.0.0.0:8000 core.wsgi:application
+exec python3 -m gunicorn --bind 0.0.0.0:8001 core.wsgi:application
